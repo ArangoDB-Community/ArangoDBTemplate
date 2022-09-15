5d0a61a0a5da86aed9af4d45bbc61754dd1c91ab
Felix Riemen <92442930+ferie24@users.noreply.github.com>
2022-09-15 13:26:01 +0200

    RTD updates

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.toctree',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
